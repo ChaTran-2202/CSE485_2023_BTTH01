4a849616d539d4e111337952178782c58ba8b321
Hiếu Nguyễn Minh <hieukd01yc@gmail.com>
2023-02-26 01:05:14 +0700

    Xu ly bai cam nhan

--- btth01_template/btth01/index.php
+++ btth01_template/btth01/index.php
@@ -1,3 +1,32 @@
+<?php
+    $songs = [
+        '',
+        'longme.jpg',
+        'vetmua.jpg',
+        'phoipha.jpg',
+        'quehuong.jpg',
+        'datnuoc.jpg',
+        'hardrock.jpg',
+        'TheUnforgiven.jpg',
+        'noitinhyeubatdau.jpg',
+        'loveme.jpg',
+        'stronger.jpg',
+        'csmt.jpg',
+        'cayvagio.jpg',
+        'nguoithay.jpg',
+    ];
+require './includes/database-connection.php';       
+                // Functions 
+$sql       = "SELECT ma_bviet, ten_bhat FROM baiviet;"; // SQL
+$statement = $pdo->query($sql);   
+
+$baiviets   = $statement->fetchAll();  
+                    
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,33 +39,8 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 bg-white rounded">
-            <div class="container-fluid">
-                <div class="my-logo">
-                    <a class="navbar-brand" href="#">
-                        <img src="images/logo2.png" alt="" class="img-fluid">
-                    </a>
-                </div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="./">Trang chủ</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="./login.php">Đăng nhập</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Nội dung cần tìm" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Tìm</button>
-                </form>
-                </div>
-            </div>
-        </nav>
-
+    <?php include './view/nav.php'; ?>
+<!-- start slider -->
         <div id="carouselExampleIndicators" class="carousel slide">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -64,68 +68,31 @@
             </button>
             </div>
     </header>
-    <main class="container-fluid mt-3">
+<!-- end slider -->
+
+    <main class=" container-fluid mt-3">
         <h3 class="text-center text-uppercase mb-3 text-primary">TOP bài hát yêu thích</h3>
-        <div class="row">
-            <div class="col-sm-3">
-                <div class="card mb-2" style="width: 100%;">
-                    <img src="images/songs/cayvagio.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">
-                            <a href="" class="text-decoration-none">Cây, lá và gió</a>
-                        </h5>
-                    </div>
-                </div>
-            </div>
 
-            <div class="col-sm-3">
-                <div class="card mb-2" style="width: 100%;">
-                    <img src="images/songs/csmt.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">
-                            <a href="" class="text-decoration-none">Cuộc sống mến thương</a>
-                        </h5>
-                    </div>
-                </div>
-            </div>
+        <div class="">
 
-            <div class="col-sm-3">
-                <div class="card mb-2" style="width: 100%;">
-                    <img src="images/songs//longme.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">
-                            <a href="" class="text-decoration-none">Lòng mẹ</a>
-                        </h5>
-                    </div>
+            <?php foreach ($baiviets as $baiviet) { ?> 
+              
+                      <div class="col-2  me-4" style="display: inline-block">
+                        <div class="card mb-2" style="width: 100%;">
+                            <img src= "images/songs/<?=$songs[$baiviet['ma_bviet']]?>"  class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">
+                                    <a href="./view/camnhan.php?id=<?= $baiviet['ma_bviet'];?>" class="text-decoration-none"> <?= $baiviet['ten_bhat'];?> </a>
+                                </h5>
+                            </div>
+                        </div> 
                 </div>
-            </div>
-
-            <div class="col-sm-3">
-                <div class="card mb-2" style="width: 100%;">
-                    <img src="images/songs/phoipha.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">
-                            <a href="" class="text-decoration-none">Phôi pha</a>
-                        </h5>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-3">
-                <div class="card mb-2" style="width: 100%;">
-                    <img src="images/songs/noitinhyeubatdau.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-center my-title">
-                            <a href="" class="text-decoration-none">Nơi tình yêu bắt đầu</a>
-                        </h5>
-                    </div>
-                </div>
-            </div>
+                  
+            <?php } ?>
         </div>
+
     </main>
-    <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
-        <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
-    </footer>
+    <?php include './view/footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
